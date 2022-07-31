4ec009097c300c619c9a6ab46603f65453d78dd0
thales.gibbon@gmail.com <thales.gibbon@gmail.com>
2022-07-31 19:52:53 -0300

    refactor name

--- storage_partnerinfo.py
+++ storage_partnerinfo.py
@@ -7,16 +7,16 @@ def storage_partnerinfo(driver_id):
     direxport = f'exports/{driver_id}/'
     os.makedirs(direxport)
 
-    partner_me = uber_lib.get_partner_me()
-    out_file = open(direxport + "partner_me.json", "w")
-    json.dump(partner_me, out_file)
+    partners_me = uber_lib.get_partners_me()
+    out_file = open(direxport + "partners_me.json", "w")
+    json.dump(partners_me, out_file)
 
-    partner_payments = uber_lib.get_partner_payments()
-    out_file = open(direxport + "partner_payments.json", "w")
-    json.dump(partner_payments, out_file)
+    partners_payments = uber_lib.get_partners_payments()
+    out_file = open(direxport + "partners_payments.json", "w")
+    json.dump(partners_payments, out_file)
 
-    partner_trips = uber_lib.get_partner_trips()
-    out_file = open(direxport + "partner_trips.json", "w")
-    json.dump(partner_trips, out_file)
+    partners_trips = uber_lib.get_partners_trips()
+    out_file = open(direxport + "partners_trips.json", "w")
+    json.dump(partners_trips, out_file)
 
     print('export feito com sucesso')
--- uber_lib.py
+++ uber_lib.py
@@ -6,13 +6,13 @@ def get_mockfile(name):
     return json.load(f)
 
 
-def get_partner_me():
+def get_partners_me():
     return get_mockfile('partners_me.json')
 
 
-def get_partner_payments():
-    return get_mockfile('partner_payments.json')
+def get_partners_payments():
+    return get_mockfile('partners_payments.json')
 
 
-def get_partner_trips():
-    return get_mockfile('partner_trips.json')
+def get_partners_trips():
+    return get_mockfile('partners_trips.json')
